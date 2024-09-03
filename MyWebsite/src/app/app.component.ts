3090b36c8b133a4d5a9eafffddca1d19621c4c7f
unknown <sivalankalaxmi809@gmail.com>
2024-09-03 21:15:23 +0530

    created a new component and routes

--- MyWebsite/src/app/app.component.ts
+++ MyWebsite/src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { AboutComponentComponent } from "./about-component/about-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive, RouterModule, AboutComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyWebsite';
+  Tag = false;
+
+  constructor(private router: Router){}
+  about() {
+    this.Tag = true;
+    console.log("About");
+    console.warn("About warning");
+    // this.router.navigate(['/about']);
+  }
 }
