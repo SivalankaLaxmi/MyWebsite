8e6ad501265f5b305fcc774e7ae6172f6af16efc
unknown <sivalankalaxmi809@gmail.com>
2025-02-19 22:38:05 +0530

    icons and contact details are added

--- MyWebsite/src/app/app.routes.ts
+++ MyWebsite/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutComponentComponent } from './about-component/about-component.component';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: 'about', component: AboutComponentComponent},
-    {path: 'home', component: AppComponent}
+    {path: 'home', component: AppComponent},
+    {path: 'contact', component: ContactComponent}
 ];
